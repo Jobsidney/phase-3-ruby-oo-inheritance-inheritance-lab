fbeb27f9a849c79cae474c5566225456682929b8
Jobsidney <jobsidney67@gmail.com>
2022-08-20 18:27:14 +0300

    Done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,22 @@
-class Student 
+require_relative "../lib/user.rb"
+
+class Student < User
+    attr_accessor :knowledge, :str
+
+    def initialize()
+        @knowledge = []
+    end
+    def learn(str)
+        @knowledge <<str
+    end
+    def first_name
+        @first
+    end
+    def last_name
+        @last
+    end
 
 end
+
+stude=Student.new()
+p stude.learn("ENlishh")
